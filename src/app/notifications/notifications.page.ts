421c8f582eabd7c3d94b52046b979dc8e54188f9
Avhashoni Netshiozwi <avhashoninetshiozwi@gmail.com>
2020-04-01 23:19:28 +0200

    2020/04/01 23:19

--- src/app/notifications/notifications.page.ts
+++ src/app/notifications/notifications.page.ts
@@ -16,7 +16,24 @@ export class NotificationsPage implements OnInit {
     public notification: any;
     public date: string;
     public unread: number = null;
-    constructor(private _service: AppService, private _modalController: ModalController) { }
+    public color: Array<{ letter: string, color?: string }> = [
+        { letter: "A" }, { letter: "B" }, { letter: "C" }, { letter: "D" },
+        { letter: "E" }, { letter: "F" }, { letter: "G" }, { letter: "H" },
+        { letter: "I" }, { letter: "J" }, { letter: "K" }, { letter: "L" },
+        { letter: "M" }, { letter: "N" }, { letter: "O" }, { letter: "P" },
+        { letter: "Q" }, { letter: "R" }, { letter: "S" }, { letter: "T" },
+        { letter: "U" }, { letter: "V" }, { letter: "W" }, { letter: "X" },
+        { letter: "Y" }, { letter: "Z" }];
+    constructor(private _service: AppService, private _modalController: ModalController) {
+        let hexArr: Array<any> = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'];
+        for (let j = 0; j < 26; j++) {
+            let color = "";
+            for (let i = 0; i < 6; i++) {
+                color += hexArr[(Math.floor(Math.random() * 16))];
+            }
+            this.color[j].color = `#${color}`;
+        }
+    }
 
     ngOnInit() {
         let locale = new DatePipe("en-ZA");
@@ -109,114 +126,9 @@ export class NotificationsPage implements OnInit {
     public colors(letter: string): string {
         letter.toUpperCase();
         let color = "";
-        switch (letter) {
-            case 'A': {
-                color = "rgba(3, 169, 244, 0.7)";
-                break;
-            }
-            case 'B': {
-                color = "rgba(244, 67, 54, 0.7)";
-                break;
-            }
-            case 'C': {
-                color = "rgba(233, 30, 99, 0.7)";
-                break;
-            }
-            case 'D': {
-                color = "rgba(156, 39, 176, 0.7)";
-                break;
-            }
-            case 'E': {
-                color = "rgba(63, 81, 181, 0.7)";
-                break;
-            }
-            case 'F': {
-                color = "rgba(0, 188, 212, 0.7)";
-                break;
-            }
-            case 'G': {
-                color = "rgba(0, 150, 136, 0.7)";
-                break;
-            }
-            case 'H': {
-                color = "rgba(76, 175, 80, 0.7)";
-                break;
-            }
-            case 'I': {
-                color = "rgba(205, 220, 57, 0.7)";
-                break;
-            }
-            case 'J': {
-                color = "rgba(255, 235, 59, 0.7)";
-                break;
-            }
-            case 'K': {
-                color = "rgba(255, 152, 0, 0.7)";
-                break;
-            }
-            case 'L': {
-                color = "rgba(121, 85, 72, 0.7)";
-                break;
-            }
-            case 'M': {
-                color = "rgba(158, 158, 158, 0.7)";
-                break;
-            }
-            case 'N': {
-                color = "rgba(96, 125, 139, 0.7)";
-                break;
-            }
-            case 'O': {
-                color = "rgba(0, 0, 0, 0.7)";
-                break;
-            }
-            case 'P': {
-                color = "rgba(62, 69, 81, 0.7)";
-                break;
-            }
-            case 'Q': {
-                color = "#1c2a48";
-                break;
-            }
-            case 'R': {
-                color = "#37474f";
-                break;
-            }
-            case 'S': {
-                color = "#ff8f00";
-                break;
-            }
-            case 'T': {
-                color = "#3F729B";
-                break;
-            }
-            case 'U': {
-                color = "";
-                break;
-            }
-            case 'V': {
-                color = "";
-                break;
-            }
-            case 'W': {
-                color = "";
-                break;
-            }
-            case 'X': {
-                color = "";
-                break;
-            }
-            case 'Y': {
-                color = "";
-                break;
-            }
-            case 'Z': {
-                color = "";
-                break;
-            }
-            default:
-                break;
-        }
+        this.color.forEach(c => {
+            c.letter == letter ? color = c.color : null;
+        });
         return color;
     }
 }
